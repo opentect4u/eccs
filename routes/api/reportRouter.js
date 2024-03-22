9fbffab562441b77c2a322de5184fe83673ad2fd
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-03-22 11:53:46 +0530

    add suc o in report 22.03.2024

--- routes/api/reportRouter.js
+++ routes/api/reportRouter.js
@@ -7,7 +7,15 @@ reportRouter.get("/demand_report", async (req, res) => {
   // console.log(data);
   var demand_details = await report_demand(data.tb_name,data.member_id,data.month,data.year)
   // console.log(demand_details);
-  res.send(demand_details);
+  if(demand_details.suc > 0){
+    if(demand_details.msg.length > 0){
+       res.send(demand_details)
+    }else{
+       res.send({suc: 0, msg: "Data not found"})
+    }
+  }else{
+     res.send(demand_details)
+  }
 });
 
 reportRouter.get("/networth_report", async (req, res) => {
@@ -15,7 +23,15 @@ reportRouter.get("/networth_report", async (req, res) => {
   // console.log(data);
   var networth_details = await report_networth(data.tb_name,data.member_id,data.month,data.year)
   // console.log(networth_details);
-  res.send(networth_details);
+  if(networth_details.suc > 0){
+    if(networth_details.msg.length > 0){
+       res.send(networth_details)
+    }else{
+       res.send({suc: 0, msg: "Data not found"})
+    }
+  }else{
+     res.send(networth_details)
+  }
 })
 
 module.exports = { reportRouter };
