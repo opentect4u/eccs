d3468b029f4410db4f880017699b80a1fece112c
sayantika dhar <sayantika.dhar04@gmail.com>
2024-05-31 08:22:24 +0530

    doing member detaiks in admin section 31.05.2024

--- modules/admin/Member_adminModule.js
+++ modules/admin/Member_adminModule.js
@@ -1,26 +1,50 @@
 const { db_Select } = require("../MasterModule");
 
 module.exports = {
-    memData : (member_id) =>{
-        return new Promise(async (resolve, reject) => {
-            var fields = "a.member_id,a.member_name,a.gurd_name,a.memb_addr,a.gender,a.dob,a.doa,a.designation,a.pf_no,b.branch_name",
-              table_name = "md_member a, md_branch b",
-              where = `a.branch_code = b.sl_no AND a.member_id = '${member_id}'`,
-              order = null;
-            var resDt = await db_Select(fields, table_name, where, order);
-            resolve(resDt);
-          });
-    },
+  memData: (id) => {
+    return new Promise(async (resolve, reject) => {
+      var fields =
+          "a.member_id,a.member_name,a.gurd_name,a.memb_addr,a.gender,a.dob,a.doa,a.designation,a.pf_no,b.branch_name",
+        table_name = "md_member a, md_branch b",
+        where = `a.branch_code = b.sl_no AND a.member_id = '${id}'`,
+        order = null;
+      var resDt = await db_Select(fields, table_name, where, order);
+      resolve(resDt);
+    });
+  },
 
-    member_dt : (data) =>{
-      return new Promise(async (resolve, reject) => {
-        var fields = "a.member_id,a.member_name,a.gurd_name,a.memb_addr,a.gender,a.dob,a.doa,a.designation,a.pf_no,b.branch_name",
-          table_name = "md_member a, md_branch b",
-          where = `a.branch_code = b.sl_no AND a.pf_no = '${data.pf_no}'`,
-          order = null;
-        var resDt = await db_Select(fields, table_name, where, order);
-        console.log(resDt,'sp');
-        resolve(resDt);
-      });
-    }
-}
+  member_dt: (data) => {
+    return new Promise(async (resolve, reject) => {
+      var fields =
+          "a.member_id,a.member_name,a.gurd_name,a.memb_addr,a.gender,a.dob,a.doa,a.designation,a.pf_no,b.branch_name",
+        table_name = "md_member a, md_branch b",
+        where = `a.branch_code = b.sl_no AND a.pf_no = '${data.pf_no}'`,
+        order = null;
+      var resDt = await db_Select(fields, table_name, where, order);
+      console.log(resDt, "sp");
+      resolve(resDt);
+    });
+  },
+  branch_list: () => {
+    return new Promise(async (resolve, reject) => {
+      var fields = "a.branch_name",
+        table_name = "md_branch a, md_member b",
+        where = `a.sl_no = b.branch_code`,
+        order = null;
+      var resDt = await db_Select(fields, table_name, where, order);
+      console.log(resDt);
+      resolve(resDt);
+    });
+  },
+
+  memberData: (id) => {
+    return new Promise(async (resolve, reject) => {
+      var fields = "a.*,b.branch_name",
+        table_name = "md_member a, md_branch b",
+        where = id > 0 ? `a.member_id = ${id}` : null,
+        order = null;
+      var resDt = await db_Select(fields, table_name, where, order);
+      resolve(resDt);
+    });
+  },
+};
--- routes/admin/memberRouter.js
+++ routes/admin/memberRouter.js
@@ -1,45 +1,107 @@
-const { memData, member_dt } = require('../../modules/admin/Member_adminModule');
+const {
+  db_Select,
+  db_Insert,
+  db_Delete,
+} = require("../../modules/MasterModule");
+const {
+  memData,
+  member_dt,
+  branch_list,
+  memberData,
+} = require("../../modules/admin/Member_adminModule");
 
-const memberRouter = require('express').Router();
+const memberRouter = require("express").Router();
 
 memberRouter.get("/member", async (req, res) => {
-    var member_id = req.query.member_id > 0 ? req.query.member_id : null;
-    var resDt = await memData(member_id);
-    console.log(resDt,'123');
-    res.render("member_dtls/member_view", {
-      mem_dt: resDt,
-      heading: "Member Details",
-      sub_heading: "Member Details List",
-      dateFormat,
-    });
+  var id = req.query.member_id > 0 ? req.query.member_id : null;
+  var resDt = await memData(id);
+  console.log(resDt, "123");
+  res.render("member_dtls/member_view", {
+    mem_dt: resDt,
+    heading: "Member Details",
+    sub_heading: "Member Details List",
+    dateFormat,
   });
+});
 
-  memberRouter.post("/member_dtls", async (req, res) => {
-    var data = req.body
-    console.log(data,'oooo');
-    var resDt = await member_dt(data);
-    console.log(resDt,'ps');
-    res.send(resDt);
+memberRouter.post("/member_dtls", async (req, res) => {
+  var data = req.body;
+  // console.log(data, "oooo");
+  var resDt = await member_dt(data);
+  console.log(resDt);
+  res.send(resDt);
+});
+
+memberRouter.get("/member_edit", async (req, res) => {
+  var id = req.query.member_id > 0 ? req.query.member_id : null;
+  var memDt = null;
+  var branch_lt = await branch_list();
+  if (id > 0) {
+    var res_dt = await memberData(id);
+    memDt = res_dt.suc > 0 ? res_dt.msg : null;
+    console.log(memDt, "123");
+  }
+  res.render("member_dtls/member_edit", {
+    mem_dt: branch_lt.suc > 0 ? branch_lt.msg : [],
+    mem_data: memDt.suc > 0 ? memDt.msg : [],
+    heading: "Member Details",
+    sub_heading: `Member Details List ${id > 0 ? "Edit" : "Add"}`,
+    breadcrumb: `<ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/admin/member">Member Details list</a></li> 
+        <li class="breadcrumb-item active">Member Details List ${
+          id > 0 ? "Edit" : "Add"
+        } </li>
+        </ol>`,
+    dateFormat,
   });
+});
 
-  memberRouter.get("/member_edit", async (req, res) => {
-    var id = req.query.id > 0 ? req.query.id : null;
-    // var conDt = null;
-    // if (id > 0) {
-    //     var res_dt = await conData(id);
-    //     conDt = res_dt.suc > 0 ? res_dt.msg : null;
-    //     console.log(conDt,'123');
-    // }
-    res.render("member_dtls/member_edit", {
-        // con_data: conDt,
-        // heading: "Contact Details",
-        // sub_heading: `Contact Details List ${id > 0 ? 'Edit' : 'Add'}`,
-        // breadcrumb: `<ol class="breadcrumb">
-        // <li class="breadcrumb-item"><a href="/admin/contact">Contact Details list</a></li> 
-        // <li class="breadcrumb-item active">Contact Details List ${id > 0 ? 'Edit' : 'Add'} </li>
-        // </ol>`,
-        dateFormat,
-    });
-});  
+memberRouter.post("/member_dtls_save", async (req, res) => {
+  var data = req.body;
+  console.log(data, "iiii");
+  var datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss"),
+    user = req.session.user.user_name,
+    id = data.member_id;
 
-module.exports = {memberRouter}
+  var table_name = "md_member",
+    fields =
+      id > 0
+        ? `branch_code = '${data.brn_name}', member_name = '${data.mem_nm}',
+      gurd_name= '${data.gurd_nm}', memb_addr= '${data.mem_addr}', gender = '${data.gen}', dob = '${data.dob}', 
+      doa = '${data.doa}', emp_code = '${data.emp_code}', designation = '${data.desg}', pf_no = '${data.pf_no}', 
+      email_id = '${data.email_id}', phone_no = '${data.phone}', modified_by = '${user}', modified_dt = '${datetime}'`
+        : "(member_id, branch_code, member_name, gurd_name, memb_addr, gender, dob, doa, emp_code, designation, pf_no, email_id, phone_no, created_by, created_dt)",
+    values = `('${data.mem_id}', '${data.brn_name}', '${data.mem_nm}', '${data.gurd_nm}', '${data.mem_addr}',
+      '${data.gen}', '${data.dob}', '${data.doa}', '${data.emp_code}', '${data.desg}', '${data.pf_no}', '${data.email_id}',
+      '${data.phone}', '${user}', '${datetime}')`,
+    whr = id > 0 ? `member_id = ${id}` : null,
+    flag = id > 0 ? 1 : 0;
+
+  var resDt = await db_Insert(table_name, fields, values, whr, flag);
+  if (resDt.suc > 0) {
+    req.session.message = {
+      type: "success",
+      message: "Successfully Saved",
+    };
+    res.redirect("/admin/member");
+    // res_dt = { suc: 1, msg: resDt.msg };
+  } else {
+    // res_dt = { suc: 0, msg: "You have entered a wrong PIN" };
+    req.session.message = {
+      type: "danger",
+      message: "Data Not Inserted!!",
+    };
+    res.redirect("/admin/member_edit?id=" + id);
+  }
+});
+
+memberRouter.get("/mem_data_delete", async (req, res) => {
+  var data = req.query;
+  console.log(data, "po");
+  var table_name = "md_member",
+    whr = `member_id=${data.member_id}`;
+  var res_dt = await db_Delete(table_name, whr);
+  res.redirect("/admin/member");
+});
+
+module.exports = { memberRouter };
